ea29cbeb1005fab9eba7ea094761173ad969febe
TangGib <530445063@qq.com>
2024-05-04 17:07:06 +0800

    Site updated: 2024-05-04 17:07:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/03/Hbase命令/","2024/05/02/hello-world/","2024/05/03/hive/"];function toRandomPost(){
+var posts=["2024/05/03/Hbase命令/","2024/05/02/hello-world/","2024/05/03/hive/","2024/05/04/你好世界/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
