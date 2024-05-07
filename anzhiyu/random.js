b038c4552dcf27666252188706ac7ee9737227b1
TangGib <530445063@qq.com>
2024-05-07 16:59:07 +0800

    Site updated: 2024-05-07 16:59:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/04/Hbase命令/","2024/05/02/hello-world/","2024/05/04/hive/","2024/05/04/你好世界/","2024/05/06/配置nginx/"];function toRandomPost(){
+var posts=["2024/05/04/Hbase命令/","2024/05/02/hello-world/","2024/05/04/hive/","2024/05/04/你好世界/","2024/05/07/考试/","2024/05/06/配置nginx/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
